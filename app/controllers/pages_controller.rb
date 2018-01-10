c1a17da1567e32f50535c38dd6864506fe6ae29f
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-10 14:00:49 -0500

    add kittens page, DRY up code and redirect with flash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,24 @@
 class PagesController < ApplicationController
+
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
   def welcome
     @header = "This is the welcome page"
   end
+
   def kitten
-  requested_size = params[:size]
-  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
-end
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
 end
